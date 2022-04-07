01c8c1ef994238add97de59e501986dad5c4684a
avijitsaha118 <sahaavijit908@gmail.com>
2022-04-07 12:20:30 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,111 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import app from './firebase.init';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 
+const auth = getAuth(app);
 function App() {
+  const [validated, setValidated] = useState(false);
+  const [registered, setRegistered]= useState(false);
+  const [error, setError] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleEmailBlur = event => {
+    setEmail(event.target.value);
+  }
+
+  const handlePasswordBlur = event => {
+    setPassword(event.target.value);
+  }
+const handleRegisteredChange = event =>{
+     setRegistered(event.target.checked);
+}
+
+  const handleFormSubmit = event => {
+
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+
+      event.stopPropagation();
+      return;
+    }
+    if (!/(?=.*?[#?!@$%^&*-])/.test(password)) {
+      setError('please password make valid by one special char')
+      return;
+    }
+    setValidated(true);
+    setError('');
+
+    if(registered){
+          signInWithEmailAndPassword(auth, email, password)
+          .then(result=>{
+            const user = result.user;
+            console.log(user);
+    })
+    .catch(error=>{
+      console.error(error);
+      setError(error.message);
+    })
+  }
+    else{
+      createUserWithEmailAndPassword(auth, email, password)
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+        setEmail('');
+        setPassword('');
+      })
+      .catch(error => {
+        console.error(error);
+        setError(error.message);
+      })
+    }
+
+
+    event.preventDefault();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <div className='registration w-50 mx-auto mt-3'>
+        <h2 className='text-primary'>Please {registered? 'Login' : 'Register'}</h2>
+        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+            <Form.Control.Feedback type="invalid">
+              Please choose a email.
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" required />
+            <Form.Control.Feedback type="invalid">
+              Please choose a password.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Check onChange={handleRegisteredChange}
+          type="checkbox"
+          label="Already Register?"
+        />
+          
+          <p className='text-danger'>
+            {error}
+          </p>
+          <Button variant="primary" type="submit">
+            {registered? 'Login': 'Register'}
+          </Button>
+        </Form>
+      </div>
+
     </div>
   );
 }
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDuIwnOowkYqZ9ap1uxlO5KdyIZQAN1xX4",
+  authDomain: "email-password-auth-3aa73.firebaseapp.com",
+  projectId: "email-password-auth-3aa73",
+  storageBucket: "email-password-auth-3aa73.appspot.com",
+  messagingSenderId: "772479734553",
+  appId: "1:772479734553:web:395b134867a27ca0186b88"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
